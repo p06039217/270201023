089991041e9b4a866ab2667de603b6fa80da74e0
p06039217 <cagataygencturk.123@gmail.com>
2020-12-14 09:00:16 +0000

    lab8

--- lab8/sumOflist_withfuntion.py
+++ lab8/sumOflist_withfuntion.py
@@ -1,8 +1,8 @@
 a_list = [12, -7, 5, -89.4, 3, 27, 56, 57.3]
-def sum():
+def sum(numbers):
     sumof = 0
     for i in a_list:
         sumof += i
     return (sumof)**2
 
-print(sum())
+print(sum(a_list))
